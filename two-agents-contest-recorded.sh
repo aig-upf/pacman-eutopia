9d00bfe57dd0a9e29ca66ee06619157891551d95
Javier Segovia <jsego6@gmail.com>
2022-03-15 09:01:23 +0100

    Issue #3: two agents contest script

---
+++ two-agents-contest-recorded.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J prova_eutopia_contest
+#SBATCH -p short
+#SBATCH -N 1
+#SBATCH -n 2 
+#SBATCH --chdir=/homedtic/jsegovia/pacman-contest-agent
+#SBATCH --time=2:00
+#SBATCH -o %N.%J.out # STDOUT
+#SBATCH -e %N.%j.err # STDERR
+
+source ../venv/bin/activate
+ml Tkinter
+python3 capture.py -b agents/team_name_1/myTeam.py -r agents/team_name_2/myTeam.py  --record --game_id 1234
+deactivate
+
