bf33d760bc75a16ee5a3bb0abcede65a171cdffd
Javier Segovia <jsego6@gmail.com>
2022-03-25 14:34:47 +0100

    Issue #8: new vs all ready; skeleton to submit cluster script

--- src/contest_manager.py
+++ src/contest_manager.py
@@ -1,15 +1,19 @@
+import random
+
 from teams_parser import TeamsParser
 import json
 from git import Repo
 import os
+from team import Team
+from typing import List
 
 
 class ContestManager:
     contests: dict
 
-    def __init__(self, json_file: str = ""):
+    def __init__(self, contests_json_file: str = ""):
         self.contests = {}
-        with open(json_file, "r") as f:
+        with open(contests_json_file, "r") as f:
             json_contests = json.load(f)
             for contest_name in json_contests:
                 self.contests[contest_name] = TeamsParser(json_file="teams_" + contest_name + ".json")
@@ -20,6 +24,7 @@ class ContestManager:
                 repo_local_dir = self.get_repo_dir(contest_name, team)
                 print(repo_local_dir, self.repo_exists(repo_local_dir))
                 if not self.repo_exists(repo_local_dir):
+                    setattr(team, "last_commit", "")
                     repo = self.clone_repo(url=team.get_repository(), dest_folder=repo_local_dir)
                 else:
                     repo = self.get_cloned_repo(dest_folder=repo_local_dir)
@@ -67,12 +72,37 @@ class ContestManager:
         with open(dest_file_name, "w") as f:
             f.write(json.dumps(self.contests[contest_name].to_json_obj()))
 
+    # def get_new_teams(self, contest_name: str) -> List[Team]:
+    #     """Return the list of new/updated teams of a given contest"""
+    #     assert contest_name in self.contests
+    #     return [team for team in self.contests[contest_name].get_teams() if team.get_updated()]
+
+    def get_all_teams(self, contest_name: str) -> List[Team]:
+        """Returns all teams of a given contest"""
+        assert contest_name in self.contests
+        return self.contests[contest_name].get_teams()
+
+    @staticmethod
+    def submit_match(blue_team: Team, red_team: Team) -> None:
+        """Call the two agents Slurm script"""
+        print(f"Slurm task: blue={blue_team.get_name()} vs red={red_team.get_name()}")
+
 
 def main():
     # teams_parser = TeamsParser(json_file="teams_upf-ai22.json")
     # print(teams_parser)
-    contest_manager = ContestManager(json_file="contests.json")
+    contest_manager = ContestManager(contests_json_file="contests.json")
     for contest_name in contest_manager.get_contest_names():
+        all_teams = contest_manager.get_all_teams(contest_name=contest_name)
+        for t1_idx in range(0, len(all_teams)):
+            for t2_idx in range(t1_idx+1, len(all_teams)):
+                # Allow only new vs all (not old vs old)
+                if not all_teams[t1_idx].get_updated() and not all_teams[t2_idx].get_updated():
+                    continue
+                new_match = [all_teams[t1_idx], all_teams[t2_idx]]
+                random.shuffle(new_match)  # randomize blue vs red
+                contest_manager.submit_match(blue_team=new_match[0], red_team=new_match[1])
+
         contest_manager.dump_json_file(contest_name=contest_name, dest_file_name=f"teams_{contest_name}.json")
 
 
