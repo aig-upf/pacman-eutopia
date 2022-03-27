a268e5429822edc393331bd0ad0a4e1b61e91a83
js <jsego6@gmail.com>
2022-03-27 17:46:13 +0200

    Pacman contest as submodule (updatable)

--- scripts/submodule_update.sh
+++ scripts/submodule_update.sh
@@ -1,2 +1,2 @@
-# git submodule update --init --remote --recursive  # looks for origin/master instead of origin/main
-git submodule foreach git pull origin main
+git submodule update --init --remote --recursive  # looks for origin/master instead of origin/main
+# git submodule foreach git pull origin main
